99c154f2ba435269294057fca4a83f39a9f49f4b
Mel Belachew <melbelachew@gmail.com>
2019-08-11 19:58:56 -0400

    Updates

---
+++ precache-manifest.57f4399e77b3f5f06cd1ed01848e3edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d36a248923077796a31d39746b82ba3",
+    "url": "/GeolocationSeasons/index.html"
+  },
+  {
+    "revision": "5f3846363da4873f2d99",
+    "url": "/GeolocationSeasons/static/css/main.3a33208c.chunk.css"
+  },
+  {
+    "revision": "8a2f910d33371bee7db7",
+    "url": "/GeolocationSeasons/static/js/2.dcc8534d.chunk.js"
+  },
+  {
+    "revision": "5f3846363da4873f2d99",
+    "url": "/GeolocationSeasons/static/js/main.4974c684.chunk.js"
+  },
+  {
+    "revision": "4f7c2ada376529c88706",
+    "url": "/GeolocationSeasons/static/js/runtime~main.edd68022.js"
+  }
+]);
